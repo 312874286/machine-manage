28df342e8013742c1035650c6a2717ce71a3e474
yangsongmao <yangsongmao@inno72.com>
2018-07-05 11:25:31 +0800

    bbbb

--- src/services/authorityManage/api.js
+++ src/services/authorityManage/api.js
@@ -2,7 +2,7 @@ import { map } from '../../utils/fetch/api';
 
 // API Doc: http://wiki.nblow.cn/pages/viewpage.action?pageId=11797216
 const api = map({
-  getTestList: '/paas/newGoodsOperation/getGoodsOperations?keyword={keyword}&pageNo={pageNo}',
+  getTestList: '/account/getTestList?keyword={keyword}&pageNo={pageNo}',
 });
 
 export default api;
