0cfa0a4cd0b952b36007caf8cf669e796fab5e4f
xiaomin Mac <1245341830@qq.com>
2018-07-09 15:33:56 +0800

    fix setGame

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -1,9 +1,5 @@
 const environment = {
-<<<<<<< HEAD
   host: 'http://192.168.33.248:30110',
-=======
-  host: '',
->>>>>>> 0756276410880b439984be06794792c73a80bbdf
   appId: 'dingoa25um8bzdtan7hjgw',
   frontHost: 'http://erp.72solo.com',
 };
