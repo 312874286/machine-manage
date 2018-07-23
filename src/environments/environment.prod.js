296b08a2853886f73b672794ec2b413ac90f2654
Ace <jiajiuxing@inno72.com>
2018-07-23 15:22:44 +0800

    修改prod配置文件

--- src/environments/environment.prod.js
+++ src/environments/environment.prod.js
@@ -1,7 +1,7 @@
 const environment = {
-  host: '//api.erp.prod.32solo.com',
+  host: 'https://api.erp.prod.32solo.com',
   appId: 'dingoakb41ozzxedr3gnpv',
-  frontHost: '//erp.prod.32solo.com',
+  frontHost: 'https://erp.prod.32solo.com',
 };
 
 export default environment;
