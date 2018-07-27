51d6b414a6a65c7dbe82472021e1a01859301dc5
xiaomin Mac <1245341830@qq.com>
2018-07-27 13:33:09 +0800

    fix schedule check/user

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -1,9 +1,5 @@
 const environment = {
-<<<<<<< HEAD
   host: 'http://172.16.19.71:8880',
-=======
-  host: '',
->>>>>>> 59e034cf406c559d3e2f018da6cda57cd9ab0bfe
   appId: 'dingoa25um8bzdtan7hjgw',
   frontHost: 'http://erp.72solo.com',
 };
