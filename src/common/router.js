a2f4af5f958f6ed3e54171f7189b976008c71c60
xiaomin Mac <1245341830@qq.com>
2018-08-06 18:49:55 +0800

    update userManage/updateStatus

--- src/common/router.js
+++ src/common/router.js
@@ -89,18 +89,18 @@ export const getRouterData = (app) => {
     // '/exception/trigger': {
     //   component: dynamicWrapper(app, [], () => import('../routes/Exception/triggerException')),
     // },
-    '/homePage': {
-      component: dynamicWrapper(app, ['polling/user', 'log/log', 'common'], () => import('../routes/HomePage/HomePage')),
-    },
-    '/offline': {
-      component: dynamicWrapper(app, ['project/channelSetting', 'log/log', 'common'], () => import('../routes/HomePage/OffLine')),
-    },
-    '/stockOut': {
-      component: dynamicWrapper(app, ['project/channelSetting', 'log/log', 'common'], () => import('../routes/HomePage/StockOut')),
-    },
-    '/unusual': {
-      component: dynamicWrapper(app, ['project/channelSetting', 'log/log', 'common'], () => import('../routes/HomePage/Unusual')),
-    },
+    // '/homePage': {
+    //   component: dynamicWrapper(app, ['polling/user', 'log/log', 'common'], () => import('../routes/HomePage/HomePage')),
+    // },
+    // '/offline': {
+    //   component: dynamicWrapper(app, ['project/channelSetting', 'log/log', 'common'], () => import('../routes/HomePage/OffLine')),
+    // },
+    // '/stockOut': {
+    //   component: dynamicWrapper(app, ['project/channelSetting', 'log/log', 'common'], () => import('../routes/HomePage/StockOut')),
+    // },
+    // '/unusual': {
+    //   component: dynamicWrapper(app, ['project/channelSetting', 'log/log', 'common'], () => import('../routes/HomePage/Unusual')),
+    // },
     '/user': {
       component: dynamicWrapper(app, [], () => import('../layouts/UserLayout')),
     },
