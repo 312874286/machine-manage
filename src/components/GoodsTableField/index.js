973ea6fb7ef75b7a8a292138d8969ff172c69e9a
xiaomin Mac <1245341830@qq.com>
2018-08-28 20:52:27 +0800

    update

--- src/components/GoodsTableField/index.js
+++ src/components/GoodsTableField/index.js
@@ -320,7 +320,7 @@ class GoodsTableField extends Component {
         dataIndex: 'shopName',
         render: (text, record) => {
           return (
-            <Select onChange={this.handleShopChangeName} defaultValue={ record.shopName } placeholder="请选择店铺">
+            <Select onSelect={this.handleShopChangeName} defaultValue={ record.shopName } placeholder="请选择店铺">
               {/*{children}*/}
               {shopClist.map((item) => {
                 return (
@@ -335,7 +335,7 @@ class GoodsTableField extends Component {
         dataIndex: 'name',
         render: (text, record) => {
           return (
-            <Select onChange={this.handleChangeName.bind(this, record)} defaultValue={ record.name } placeholder="请选择商品">
+            <Select onSelect={this.handleChangeName.bind(this, record)} defaultValue={ record.name } placeholder="请选择商品">
               {/*{children}*/}
               {clist.map((item) => {
                 return (
