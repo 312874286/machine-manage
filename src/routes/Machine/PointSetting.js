3b031783d0e5055e08be388fb1210f5716a0fd9e
xiaomin Mac <1245341830@qq.com>
2018-09-19 17:58:58 +0800

    update

--- src/routes/Machine/PointSetting.js
+++ src/routes/Machine/PointSetting.js
@@ -646,7 +646,7 @@ export default class PointSettingList extends PureComponent {
           remark: data.remark || '',
           monitorStart: data.monitorStart ? moment(data.monitorStart, 'HH:mm:ss') : undefined,
           monitorEnd: data.monitorEnd ? moment(data.monitorEnd, 'HH:mm:ss') : undefined,
-          type: data.type.toString()
+          type: data.type ? data.type.toString() : ''
         });
       })
     } else {
