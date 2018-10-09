d31b0c08f42af3ffc06cf0036d8b38a10fb311c4
zhanglonglong <zhanglonglong@inno72.com>
2018-10-09 15:41:38 +0800

    移除异常引用

--- src/components/Project/InteractSamplingSteps/MachinePlan/MachinePlanTable.js
+++ src/components/Project/InteractSamplingSteps/MachinePlan/MachinePlanTable.js
@@ -1,6 +1,5 @@
 import React, { PureComponent, Fragment } from "react";
 import { Table, Divider, Popconfirm } from "antd";
-import { identity } from "gl-matrix/src/gl-matrix/mat2";
 
 export default class MachinePlanTable extends PureComponent {
   state = {
@@ -74,38 +73,7 @@ export default class MachinePlanTable extends PureComponent {
 }
 
 export class MachinePlanedGoodsTable extends PureComponent {
-  renderTable = () => {
-    const columns = [
-      { title: "商品名称", dataIndex: "goodsName" },
-      { title: "商品数量", dataIndex: "number" },
-      {
-        title: "操作",
-        dataIndex: "operation",
-        render: record => {
-          return (
-            <Fragment>
-              <Popconfirm
-                title="确定要删除吗"
-                onConfirm={this.props.onDeleteGoods}
-                okText="确定"
-                cancelText="取消"
-              >
-                <a>删除</a>
-              </Popconfirm>
-            </Fragment>
-          );
-        }
-      }
-    ];
-    return (
-      <Table
-        rowKey="goodsId"
-        columns={columns}
-        dataSource={this.props.dataSource}
-        pagination={false}
-      />
-    );
-  };
+  
   render() {
     return this.renderTable();
   }
