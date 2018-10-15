d15f4ef1d75c8809d6567f7d9c85188c5e881873
xiaomin Mac <1245341830@qq.com>
2018-10-15 15:15:56 +0800

    批次管理

--- src/routes/Machine/BatchSetting.js
+++ src/routes/Machine/BatchSetting.js
@@ -9,14 +9,257 @@ import {
   message,
   Row,
   Col,
-  Input
+  Input,
+  Modal
 } from 'antd';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
 import styles from './BatchSetting.less';
 import BatchTableField from '../../components/Machine/batchTableSetting';
 import {getAccountMenus} from "../../utils/authority";
+import moment from "moment/moment";
 
 const FormItem = Form.Item;
+const CreateForm = Form.create()(
+  (props) => {
+    const { modalVisible, form, handleAdd, handleModalVisible, editModalConfirmLoading, modalType,
+      verifyTimeRequire, gameLists, activityLists, openSelectMachineModal, selectCityName, machineNum,
+      goodsInitData, goodsCount, couponsInitData, couponsCount, goodsHandle, goodsHandleAdd, goodsHandleDelete,
+      goodsHandleChange, discountHandle, discountHandleAdd, discountHandleDelete, discountHandleChange, modalData,
+      onSelectShop, goodsLists, shopClist,
+      disabledStartDate, onStartChange, disabledEndDate, onEndChange, handleStartOpenChange, handleEndOpenChange, endOpen,
+      isDisabled, selectMachineFlag, disabledTime, disabledEndTime, couponsShow, shopHandle, onSelectGame, maxNumber, remark
+    } = props;
+    const { getFieldDecorator } = form;
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 6 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 18 },
+      },
+    };
+    const goodsColumns = [{
+      title: '商品名称',
+      dataIndex: 'name',
+      align: 'center',
+    }, {
+      title: '规则',
+      dataIndex: 'resultCode',
+      align: 'center',
+    }, {
+      title: '规则描述',
+      dataIndex: 'resultRemark',
+      align: 'center',
+    }];
+    const couponsColumns = [{
+      title: 'InteractID',
+      dataIndex: 'code',
+      align: 'center',
+    }, {
+      title: '优惠券名称',
+      dataIndex: 'name',
+      align: 'center',
+    }, {
+      title: '规则',
+      dataIndex: 'resultCode',
+      align: 'center',
+    }, {
+      title: '规则描述',
+      dataIndex: 'resultRemark',
+      align: 'center',
+    }];
+    return (
+      <Modal
+        title={
+          <div class="modalBox">
+            <span class="leftSpan"></span>
+            <span class="modalTitle">{!modalType ? '编辑排期' : '新增排期'}</span>
+          </div>
+        }
+        visible={modalVisible}
+        onOk={handleAdd}
+        onCancel={() => handleModalVisible(false)}
+        confirmLoading={editModalConfirmLoading}
+        width={800} >
+        <div className="manageAppBox">
+          <Form onSubmit={this.handleSearch}>
+            <FormItem {...formItemLayout} label="选择活动" style={{ display : modalData.id ? 'none' : 'block' }}>
+              {getFieldDecorator('activityId', {
+                rules: [{ required: false, message: '请选择活动' }],
+              })(<Select placeholder="请选择" onSelect={onSelectShop} >
+                {activityLists.map((item) => {
+                  return (
+                    <Option value={item.id} key={item.id} data-id={item.id} data-type={item.type}>{item.name}</Option>
+                  );
+                })}
+              </Select>)}
+            </FormItem>
+            <FormItem {...formItemLayout} label="选择活动" style={{ display : modalData.id ? 'block' : 'none' }}>
+              {getFieldDecorator('activityName', {
+                rules: [{ required: false, message: '请选择活动' }],
+              })(<Input disabled />)}
+            </FormItem>
+            <FormItem {...formItemLayout} label="选择开始时间" style={{ display: isDisabled ? 'none' : 'block' }}>
+              {getFieldDecorator('startTimeStr', {
+                rules: [{ required: true, message: '选择开始时间' }],
+              })(
+                <DatePicker
+                  disabledDate={disabledStartDate}
+                  disabledTime={disabledTime}
+                  showTime={{ defaultValue: moment('00:00:00', 'HH:mm:ss') }}
+                  format="YYYY-MM-DD HH:mm"
+                  // value={startValue}
+                  placeholder="选择开始时间"
+                  onChange={onStartChange}
+                  onOpenChange={handleStartOpenChange}
+                />
+              )}
+            </FormItem>
+            <FormItem {...formItemLayout} label="选择开始时间" style={{ display: isDisabled ? 'block' : 'none' }}>
+              {getFieldDecorator('startTimeStr', {
+                rules: [{ required: true, message: '选择开始时间' }],
+              })(
+                <DatePicker
+                  disabledDate={disabledStartDate}
+                  showTime
+                  format="YYYY-MM-DD HH:mm"
+                  // value={startValue}
+                  placeholder="选择开始时间"
+                  onChange={onStartChange}
+                  onOpenChange={handleStartOpenChange}
+                  disabled
+                />
+              )}
+            </FormItem>
+            <FormItem {...formItemLayout} label="选择结束时间">
+              {getFieldDecorator('endTimeStr', {
+                rules: [{ required: true, message: '选择结束时间' }],
+              })(
+                <DatePicker
+                  disabledTime={disabledEndTime}
+                  disabledDate={disabledEndDate}
+                  showTime={{ defaultValue: moment('23:59:59', 'HH:mm:ss') }}
+                  format="YYYY-MM-DD HH:mm"
+                  // value={endValue}
+                  placeholder="选择结束时间"
+                  onChange={onEndChange}
+                  open={endOpen}
+                  onOpenChange={handleEndOpenChange}
+                />
+              )}
+            </FormItem>
+            <FormItem {...formItemLayout} label="选择机器">
+              {getFieldDecorator('remark', {
+                rule: [{ validator: verifyTimeRequire }],
+              }) (
+                <div>
+                  {
+                    (remark ? remark : (
+                      selectCityName.length > 0
+                        ? '已选择' + machineNum + '台机器，分别位于' + selectCityName.join('、')
+                        : ''
+                    ))
+                  }
+                  <Button type="primary" onClick={openSelectMachineModal}>+ 选择</Button>
+                </div>
+              )
+              }
+            </FormItem>
+            {/*style={{ display: isDisabled ? 'block' : 'none' }}*/}
+            <FormItem {...formItemLayout} label="选择游戏">
+              {getFieldDecorator('gameId', {
+                rules: [{ required: false, message: '请选择游戏' }],
+              })(
+                <Select placeholder="请选择" onSelect={onSelectGame}>
+                  {gameLists.map((item) => {
+                    return (
+                      <Option value={item.id} key={item.id} data-maxNumber={item.maxGoodsNum}>{item.name}</Option>
+                    );
+                  })}
+                </Select>
+              )}
+            </FormItem>
+            {/*<FormItem {...formItemLayout} label="选择游戏" style={{ display: isDisabled ? 'block' : 'none' }}>*/}
+            {/*{getFieldDecorator('gameId', {*/}
+            {/*rules: [{ required: false, message: '请选择游戏' }],*/}
+            {/*})(*/}
+            {/*<Select placeholder="请选择" disabled>*/}
+            {/*{gameLists.map((item) => {*/}
+            {/*return (*/}
+            {/*<Option value={item.id} key={item.id} >{item.name}</Option>*/}
+            {/*);*/}
+            {/*})}*/}
+            {/*</Select>*/}
+            {/*)}*/}
+            {/*</FormItem>*/}
+            <FormItem {...formItemLayout} label="同一用户获得商品次数">
+              {getFieldDecorator('userMaxTimes', {
+                rules: [{ required: false, whitespace: true, message: '请填写同一用户获得商品次数' }],
+              })(<Input placeholder="请填写同一用户获得商品次数" />)}
+            </FormItem>
+            {/*<FormItem {...formItemLayout} label="同一用户获得商品次数" style={{ display: isDisabled ? 'block' : 'none' }}>*/}
+            {/*{getFieldDecorator('userMaxTimes', {*/}
+            {/*rules: [{ required: false, whitespace: true, message: '请填写同一用户获得商品次数' }],*/}
+            {/*})(<Input placeholder="请填写同一用户获得商品次数" disabled />)}*/}
+            {/*</FormItem>*/}
+            <FormItem {...formItemLayout} label="同一用户每天获得商品总次数">
+              {getFieldDecorator('dayUserMaxTimes', {
+                rules: [{ required: false, whitespace: true, message: '请填写同一用户每天获得商品总次数' }],
+              })(<Input placeholder="请填写同一用户每天获得商品总次数" />)}
+            </FormItem>
+            {/*<FormItem {...formItemLayout} label="同一用户每天获得商品次数" style={{ display: isDisabled ? 'block' : 'none' }}>*/}
+            {/*{getFieldDecorator('dayUserMaxTimes', {*/}
+            {/*rules: [{ required: false, whitespace: true, message: '请填写同一用户获得商品次数' }],*/}
+            {/*})(<Input placeholder="请填写同一用户每天获得商品次数" disabled />)}*/}
+            {/*</FormItem>*/}
+            <FormItem label={`填写商品信息：最多可添加${maxNumber}个商品`}>
+              {/*<Table*/}
+              {/*columns={goodsColumns}*/}
+              {/*dataSource={goodsInitData}*/}
+              {/*rowKey={record => record.prizeId}*/}
+              {/*pagination={false}*/}
+              {/*style={{ display: isDisabled ? 'block' : 'none' }} />*/}
+              <div className={styles.goodsNoteBox}>
+                <FormItem label='若要重新选择商品时，需要重新选择一下店铺'>
+                  <GoodsTableField
+                    initData={goodsInitData}
+                    count={goodsCount}
+                    clist={goodsLists}
+                    shopClist={shopClist}
+                    shopHandle={shopHandle}
+                    goodsHandle={goodsHandle}
+                    goodsHandleAdd={goodsHandleAdd}
+                    goodsHandleDelete={goodsHandleDelete}
+                    goodsHandleChange={goodsHandleChange}
+                    couponsShow={!couponsShow}
+                    maxNumber={maxNumber}
+                  />
+                </FormItem>
+              </div>
+            </FormItem>
+            {/*style={{ display: couponsShow ? 'block' : 'none' }}*/}
+            <FormItem label="填写优惠券信息">
+              {/*<Table columns={couponsColumns} dataSource={couponsInitData} rowKey={record => record.code} pagination={false} style={{ display: isDisabled ? 'block' : 'none' }} />*/}
+              <div>
+                <DiscountDynamicField
+                  shopClist={shopClist}
+                  couponsShow={!couponsShow}
+                  initData={couponsInitData}
+                  count={couponsCount}
+                  discountHandle={discountHandle}
+                  discountHandleAdd={discountHandleAdd}
+                  discountHandleDelete={discountHandleDelete}
+                  discountHandleChange={discountHandleChange}
+                />
+              </div>
+            </FormItem>
+          </Form>
+        </div>
+      </Modal>
+    );
+  });
 @connect(({ common, loading, batchSetting }) => ({
   common,
   batchSetting,
@@ -196,6 +439,7 @@ export default class versionSetting extends PureComponent {
             />
           </div>
         </Card>
+        <CreateForm />
       </PageHeaderLayout>
     );
   }
