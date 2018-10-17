451d96060e14e125e6cdc532e555e5c1c5d58266
xiaomin Mac <1245341830@qq.com>
2018-10-17 16:35:21 +0800

    超时颜色处理

--- src/routes/Project/InteractSamplingSetting.js
+++ src/routes/Project/InteractSamplingSetting.js
@@ -709,7 +709,7 @@ export default class areaSettingList extends PureComponent {
         key: "realNum",
         render: (text, item) => (
           <Fragment>
-            <span>
+            <span style={{ color: parseInt(item.status) === 4 ? 'rgba(226, 10, 30, 1)' : '' }}>
               {item.realNum ? item.realNum : 0}/{item.number ? item.number : 0}
             </span>
           </Fragment>
