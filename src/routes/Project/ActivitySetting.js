a575886b3a6586506c0ccec1b99c3d17444e3f26
xiaomin Mac <1245341830@qq.com>
2018-11-02 15:30:30 +0800

    update

--- src/routes/Project/ActivitySetting.js
+++ src/routes/Project/ActivitySetting.js
@@ -1739,7 +1739,7 @@ export default class activitySettingList extends PureComponent {
         payload: {
           restParams: {
             activityId: item.id,
-            activityType: 1,
+            activityType: 0,
           }
         }
       })
