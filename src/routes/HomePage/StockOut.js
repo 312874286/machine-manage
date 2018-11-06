d903443b0896d287c643271a0bd91be08476baab
xiaomin Mac <1245341830@qq.com>
2018-11-06 16:27:12 +0800

    缺货 创建工单

--- src/routes/HomePage/StockOut.js
+++ src/routes/HomePage/StockOut.js
@@ -153,7 +153,7 @@ export default class stockOut extends PureComponent {
         fixed: 'right',
         width: 150,
         title: '操作',
-        render: () => (
+        render: (text, item) => (
           <Fragment>
             <a
               onClick={() => this.props.history.push({ pathname: '/check/fault', query: { flag: 'openFault', machine: `${item.local}—${item.machineCode}`, machineId: `${item.id}、${item.machineCode}`}})}
